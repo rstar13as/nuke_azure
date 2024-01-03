37d5153fc6d8756feb24e4e562bee36b65258f40
rstar13as <154101569+rstar13as@users.noreply.github.com>
2024-01-04 08:41:56 +1000

    Update nuke.sh

--- nuke.sh
+++ nuke.sh
@@ -1,4 +1,6 @@
 terraform destroy --auto-approve
 # az group delete --yes --no-wait -g ad-hunting-lab
 az group list --query "[].name" --output tsv | xargs -L 1 az group delete --no-wait --yes --name
+az monitor activity-log list --offset 1m --output table
+# az monitor activity-log list --offset 1m --query "[].{ResourceGroup: resourceGroup, Time: eventTimestamp, OperationName: operationName, localizedValue}" --output table
 rm -rf terraform.tfstate*
